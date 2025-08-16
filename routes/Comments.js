fb735c418e87a84e2fd6631a88bc4b0238bc7057
Aborisade <bobleeswaggs123@gmail.com>
2025-08-16 17:34:11 +0100

    Feat: more updates to the BE, for deleting comment

--- routes/Comments.js
+++ routes/Comments.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { Comments } = require('../models');
 const { validateToken } = require('../middlewares/Authmiddleware');
+const { where } = require('sequelize');
 
 router.get('/:postId', async (req, res) => {
     const postId = req.params.postId
-    const post = await Comments.findAll({where: {postId: postId}}) ;
+    const post = await Comments.findAll({ where: { postId: postId } });
     res.json(post);
 })
 router.post('/', validateToken, async (req, res) => {
@@ -18,6 +19,15 @@ router.post('/', validateToken, async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message }); // ✅ Send error status
     }
+})
+router.delete('/:commentId', validateToken, async (req, res) => {
+    const commentId = req.params.commentId;
+    Comments.destroy({
+        where: {
+            id: commentId
+        }
+    })
+    res.json("Succesfully deleted");
 })
 
 module.exports = router;
